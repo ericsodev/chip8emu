9708ffb1a4738da0e4fef29044196bd4791c82e8
yatrippin <eric.so.dev@gmail.com>
2022-10-11 02:24:29 -0400

    added all instructions

--- src/Emulator.java
+++ src/Emulator.java
@@ -1,19 +1,23 @@
+import javafx.scene.input.KeyCode;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.*;
+
+import static java.util.Map.entry;
 
 public class Emulator {
-    private int[] memory;
-    private int[] stack;
-    private int[] reg;
+    private final int[] memory;
+    private final int[] stack;
+    private final int[] reg;
     private int pc;
     private int i_reg;
     private byte stack_ptr;
-    private boolean[][] displayBuffer;
-    private boolean testFlag = false;
+    private final boolean[][] displayBuffer;
+    private int delayTimer;
+    private int soundTimer;
+    private HashMap<Integer, Boolean> activeKeys;
 
     private final char[] FONTSET =
     {
@@ -35,14 +39,34 @@ public class Emulator {
             0xF0, 0x80, 0xF0, 0x80, 0x80  //F
 };
 
+    private HashMap<Integer, Integer> keycodeHexMap = new HashMap<>(Map.ofEntries(
+            entry(KeyCode.DIGIT1.getCode(), 0x1),
+            entry(KeyCode.DIGIT2.getCode(), 0x2),
+            entry(KeyCode.DIGIT3.getCode(), 0x3),
+            entry(KeyCode.DIGIT4.getCode(), 0xC),
+            entry(KeyCode.Q.getCode(), 0x4),
+            entry(KeyCode.W.getCode(), 0x5),
+            entry(KeyCode.E.getCode(), 0x6),
+            entry(KeyCode.R.getCode(), 0xD),
+            entry(KeyCode.A.getCode(), 0x7),
+            entry(KeyCode.S.getCode(), 0x8),
+            entry(KeyCode.D.getCode(), 0x9),
+            entry(KeyCode.F.getCode(), 0xE),
+            entry(KeyCode.Z.getCode(), 0xA),
+            entry(KeyCode.X.getCode(), 0x0),
+            entry(KeyCode.C.getCode(), 0xB),
+            entry(KeyCode.V.getCode(), 0xF)
+        ));
 
-    public Emulator() {
+
+    public Emulator(HashMap<Integer, Boolean> activeKeys) {
         this.memory = new int[4096];
         this.stack = new int[16];
         this.reg = new int[16];
         this.pc = 512;
         this.stack_ptr = 0;
         this.displayBuffer = new boolean[32][64];
+        this.activeKeys = activeKeys;
 
         for (int i = 0; i < 80; i++) {
             this.memory[i] = this.FONTSET[i];
@@ -66,9 +90,9 @@ public class Emulator {
 
     void cycle() {
         int instruction = (this.memory[pc] & 0xFF) << 8 | (this.memory[pc+1] & 0xFF);
+//        System.out.println("PC: " + pc + " Instr: " + String.format("0x%04X", instruction));
         this.pc += 2;
         int x, y;
-
         switch (instruction & 0xF000) {
             // Extract first 4 bits
             case 0x0000:
@@ -83,10 +107,14 @@ public class Emulator {
                         break;
                     case 0x00EE:
                         // Return, 00EE
+                        this.stack_ptr--;
+                        this.pc = this.stack[this.stack_ptr];
                         break;
                     default:
-                        // Sys Addr, 0NNN
-                        break;
+                        System.out.println("Unknown Instruction: " + instruction);
+//                    default:
+//                        // Sys Addr, 0NNN
+//                        break;
 
                 }
                 break;
@@ -95,12 +123,31 @@ public class Emulator {
                 this.pc = 0x0FFF & instruction; // 554  ends up setting pc to 552 again, inc to 554 then ...
                 break;
             case 0x2000:
+                // Push PC to stack, set pc to NNN, 2NNN
+                this.stack[stack_ptr++] = pc;
+                this.pc = 0x0FFF & instruction;
                 break;
             case 0x3000:
+                // Skip next instruction if VX == NN, 3XNN
+                x = (0x0F00 & instruction) >> 8;
+                if (this.reg[x] == (0x00FF & instruction)) {
+                    pc += 2;
+                }
                 break;
             case 0x4000:
+                // Skip next instruction if VX != NN, 4XNN
+                x = (0x0F00 & instruction) >> 8;
+                if (this.reg[x] != (0x00FF & instruction)) {
+                    pc += 2;
+                }
                 break;
             case 0x5000:
+                // Skip next instruction if VX == VY, 5XY0
+                x = (0x0F00 & instruction) >> 8;
+                y = (0x00F0 & instruction) >> 4;
+                if (this.reg[x] == this.reg[y]) {
+                    pc += 2;
+                }
                 break;
             case 0x6000:
                 // Set register VX, 6XNN
@@ -108,7 +155,8 @@ public class Emulator {
                 break;
             case 0x7000:
                 // Add register VX, 7XNN
-                this.reg[(0x0F00 & instruction) >> 8] += 0x00FF & instruction;
+                x = (0x0F00 & instruction) >> 8;
+                this.reg[x] = (this.reg[x] + (0x00FF & instruction)) &0xFF;
                 break;
             case 0x8000:
                 x = (0x0F00 & instruction) >> 8;
@@ -132,9 +180,11 @@ public class Emulator {
                         break;
                     case 0x0004:
                         // Set VX to (VX + VY) and set VF to carry, 8XY4
-                        this.reg[x] += this.reg[y];
-                        this.reg[0xF] = (byte) (this.reg[x] / 128);
-                        this.reg[x] %= 128;
+                        this.reg[0xF]= 0;
+                        if (reg[x] + reg[y] > 0xFF) {
+                            this.reg[0xF]= 1;
+                        }
+                        this.reg[x] = (this.reg[x] + this.reg[y]) & 0xFF;
                         break;
                     case 0x0005:
                         // Set VX to (VX - VY) and set VF to carry, 8XY5
@@ -142,7 +192,7 @@ public class Emulator {
                         if (this.reg[y] > this.reg[x]) {
                             this.reg[0xF] = 0;
                         }
-                        this.reg[x] = this.reg[x] - this.reg[y];
+                        this.reg[x] = (this.reg[x] - this.reg[y]) &0xFF;
                         break;
                     case 0x0007:
                         // Set VX to (VY - VX) and set VF to carry, 8XY5
@@ -150,19 +200,51 @@ public class Emulator {
                         if (this.reg[x] > this.reg[y]) {
                             this.reg[0xF] = 0;
                         }
-                        this.reg[x] = this.reg[y] - this.reg[x];
+                        this.reg[x] = (this.reg[y] - this.reg[x]) & 0xFF;
                         break;
+                    case 0x0006:
+                        // Set VX to (VY >> 1) and set VF to the shifted bit, 8XY6
+                        // Uses original COSMAC VIP instruction
+//                        this.reg[0xF] = this.reg[y] & 0x1;
+//                        this.reg[x] = this.reg[y] >> 1;
+                        // Super Chip Instruction
+                        this.reg[0xF] = this.reg[x] & 0x1;
+                        this.reg[x] >>= 1;
+                        break;
+                    case 0x000E:
+                        // Set VX to (VY << 1) and set VF to the shifted bit, 8XYE
+                        // Uses original COSMAC VIP instruction
+//                        this.reg[0xF] = (this.reg[x] & 0x80) >> 7;
+//                        this.reg[x] = (this.reg[y] << 1) & 0xFF;
+                        // Super Chip Instruction
+                        this.reg[0xF] = this.reg[x] >> 7;
+                        this.reg[x] <<= 1;
+                        break;
+                    default:
+                        System.out.println("Unknown Instruction: " + instruction);
                 }
                 break;
             case 0x9000:
+                // Skip next instruction if VX != VY, 9XY0
+                x = (0x0F00 & instruction) >> 8;
+                y = (0x00F0 & instruction) >> 4;
+                if (this.reg[x] != this.reg[y]) {
+                    this.pc += 2;
+                }
                 break;
             case 0xA000:
                 // Set Reg I, ANNN
                 this.i_reg = 0x0FFF & instruction;
                 break;
             case 0xB000:
+                // Jump to NNN + V0, BNNN
+                this.pc = (0x0FFF & instruction) + this.reg[0];
                 break;
             case 0xC000:
+                // Set VX to RANDNUM & 0xNN, 0xCXNN
+                x = (0x0F00 & instruction) >> 8;
+                Random rand = new Random();
+                this.reg[x] = rand.nextInt(0, 255) & (0x00FF & instruction);
                 break;
             case 0xD000:
                 // Draw, DXYN
@@ -188,17 +270,117 @@ public class Emulator {
                         }
                     }
                 }
-                testFlag = true;
                 break;
             case 0xE000:
+                x = (0x0F00 & instruction) >> 8;
+                int keycode = -1;
+                for (Map.Entry<Integer, Integer> entry : keycodeHexMap.entrySet()) {
+                    if (entry.getValue().equals(this.reg[x])) keycode = entry.getKey();
+                }
+                switch (0x00FF & instruction) {
+                    case 0x009E:
+                        // Skip next instruction if key in VX is being pressed, EX9E
+                        if (activeKeys.containsKey(keycode)) {
+                            pc += 2;
+                        }
+                        break;
+                    case 0x00A1:
+                        // Skip next instruction if key in VX is not pressed, EXA1
+                        if (!activeKeys.containsKey(keycode)) {
+                            pc += 2;
+                        }
+                        break;
+                    default:
+                        System.out.println("Unknown Instruction: " + instruction);
+                }
                 break;
             case 0xF000:
+                x = (0x0F00 & instruction) >> 8;
+                switch (0x00FF & instruction) {
+                    case 0x0007:
+                        // Set VX to delay timer, FX07
+                        this.reg[x] = this.delayTimer;
+                        break;
+                    case 0x0015:
+                        // Set delay timer to VX, FX15
+                        this.delayTimer = this.reg[x];
+                        break;
+                    case 0x0018:
+                        // Set sound timer to VX, FX18
+                        this.soundTimer = this.reg[x];
+                        break;
+                    case 0x001E:
+                        // Set I to VX + I, use VF as carry, FX1E
+                        // Assume I is a 12 bit register, memory addresses only go up to 4095.
+                        this.reg[0xF] = 0;
+                        if (i_reg + reg[x] > 0xFFF) {
+                            this.reg[0xF] = 1;
+                        }
+                        this.i_reg = (this.reg[x] + this.i_reg) & 0xFFF;
+                        break;
+                    case 0x000A:
+                        // Block until a key is pressed, store key into VX, FX0A
+                        int key = -1;
+                        for (int i = 0; i < 50; i++) {
+                            if (activeKeys.isEmpty()) continue;
+                            Set<Integer> activeSet = new HashSet<Integer>(activeKeys.keySet());
+                            activeSet.retainAll(keycodeHexMap.keySet());
+                            if (activeSet.isEmpty()) continue;
+                            key = keycodeHexMap.getOrDefault(activeSet.toArray()[0], -1);
+                            System.out.println(key);
+                            break;
+                        }
+
+                        if (key == -1) {
+                            pc -= 2; // Repeat this instruction
+                        }
+                        this.reg[x] = key;
+
+                        break;
+                    case 0x0029:
+                        // Set I to the memory address of the hex character in VX, FX29
+                        // Each 4x5 font is stored in order starting from 0x0.
+                        this.i_reg = this.reg[x] * 0x5;
+                        break;
+                    case 0x0033:
+                        // Decimal conversion, FX33
+                        // Store third digit in mem[I], second in mem[I + 1], first in mem[I + 2]
+                        this.memory[this.i_reg + 2] = this.reg[x]  % 10;
+                        this.memory[this.i_reg + 1] = (this.reg[x]  / 10) % 10;
+                        this.memory[this.i_reg] = this.reg[x]  / 100;
+                        break;
+                    case 0x0055:
+                        // Save V0 to VX to memory at I to I + X, FX55
+                        System.arraycopy(this.reg, 0, this.memory, this.i_reg, x + 1);
+//                        this.i_reg += x + 1; // Used for original interpreter
+                        break;
+                    case 0x0065:
+                        // Load memory from I to I + X to V0 to VX, FX65
+                        System.arraycopy(this.memory, this.i_reg, this.reg, 0, x + 1);
+//                        this.i_reg += x + 1; // Used for original interpreter
+                        break;
+                    default:
+                        System.out.println("Unknown Instruction: " + instruction);
+                }
                 break;
+            default:
+                System.out.println("Unknown Instruction: " + instruction);
 
         }
     }
-    boolean getTestFlag() {
-        return this.testFlag;
+    void decrementDelayTimer() {
+        this.delayTimer--;
     }
 
+    void decrementSoundTimer() {
+        this.soundTimer--;
+    }
+
+    int getDelayTimer() {
+        return this.delayTimer;
+    }
+
+    int getSoundTimer() {
+        return this.soundTimer;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
-    private ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
+    private final ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(3);
+    private HashMap<Integer, Boolean> activeKeys = new HashMap<>();
     public static void main(String[] args) {
         launch(args);
     }
@@ -19,29 +23,33 @@ public class Main extends Application {
         Display display = new Display();
         Group group = new Group(display);
         Scene scene = new Scene(group, 640, 320);
-        Emulator cpu = new Emulator();
-        cpu.loadFile("ibm.ch8");
-        ScheduledFuture<?> cpuThread = pool.scheduleWithFixedDelay(cpu::cycle, 0, 50, TimeUnit.MILLISECONDS);
+        Media sound = new Media(new File("berp.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        scene.setOnKeyPressed(e -> {
+            if (!activeKeys.containsKey(e.getCode().getCode())) {
+                activeKeys.put(e.getCode().getCode(), true);
+            }
+        });
+        scene.setOnKeyReleased(e -> {
+            activeKeys.remove(e.getCode().getCode());
+        });
+        Emulator cpu = new Emulator(activeKeys);
+        cpu.loadFile("snake.ch8");
+        ScheduledFuture<?> cpuThread = pool.scheduleWithFixedDelay(cpu::cycle, 0, 1, TimeUnit.MILLISECONDS);
+        ScheduledFuture<?> timerThread = pool.scheduleWithFixedDelay(() -> {
+            if (cpu.getDelayTimer() > 0) cpu.decrementDelayTimer();
+            if (cpu.getSoundTimer() > 0) {
+                cpu.decrementSoundTimer();
+                if (cpu.getSoundTimer() > 0) {
+                    mediaPlayer.play();
+                }
+            }
+        }, 0, 17, TimeUnit.MILLISECONDS);
         ScheduledFuture<?> displayThread = pool.scheduleWithFixedDelay(() -> {
             display.draw(cpu.getDisplayBuffer());
         }, 0, 17, TimeUnit.MILLISECONDS);
         stage.setScene(scene);
         stage.show();
-//        while (true) {
-//            cpu.cycle();
-//            display.draw(cpu.getDisplayBuffer());
-//            boolean[][] x = cpu.getDisplayBuffer();
-//            int count = 0;
-//            for (int i =0; i < 32; i++) {
-//                for (int j=0; j<64; j++) {
-//                    if (x[i][j]) count++;
-//                }
-//            }
-//            System.out.println(count);
-//        }
-
-
-
     }
     @Override
     public void stop() {
